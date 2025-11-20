1eda6564b681b35f30d3898c16defa89309f0fa0
Kaveh <kaveh.fazaeli@nn.nl>
2025-11-20 17:58:34 +0100

    Add post_gen_project.py script for project setup automation

--- pkg/hooks/pre_gen_project.sh
+++ pkg/hooks/pre_gen_project.sh
@@ -4,8 +4,7 @@ set -euo pipefail
 echo "[pre_gen] Starting"
 
 # Expect: export ORIG_COMMON_PATH=/absolute/path/to/common before cruft create
-echo $pwd
+pwd
 
 ls ../..
 
-cp -R ../../common ./common
